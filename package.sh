398f5330f3373b4258cf3ef632a7a717ee5229eb
Denis Sermukhamedov <den.medov@gmail.com>
2016-11-13 20:01:50 +0600

    add run tests with emulator on travis ci

--- package.sh
+++ package.sh
@@ -5,5 +5,5 @@ git clone https://github.com/dmedov/android-docker-ci.git
 cd android-docker-ci
 docker build -t dmedov/android .
 cd ..
-docker run --tty --interactive --volume=$(pwd):/opt/workspace --workdir=/opt/workspace --rm dmedov/android  /bin/sh -c "./gradlew test"
+docker run --tty --interactive --volume=$(pwd):/opt/workspace --workdir=/opt/workspace --rm dmedov/android  /bin/sh -c "run-tests-emulator.sh"
 
