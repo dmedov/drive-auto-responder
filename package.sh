28719869513f4c5b63cf9062e8928df899b386a2
Denis Sermukhamedov <den.medov@gmail.com>
2016-11-16 19:48:46 +0600

    move package.sh to debian branch

--- package.sh
+++ package.sh
@@ -3,6 +3,7 @@ set -e -x
 
 git clone https://github.com/dmedov/android-docker-ci.git
 cd android-docker-ci
+git checkout debian
 docker build -t dmedov/android .
 cd ..
 docker run --tty --interactive --volume=$(pwd):/opt/workspace --workdir=/opt/workspace --rm dmedov/android  /bin/sh -c "run-tests-emulator.sh"
