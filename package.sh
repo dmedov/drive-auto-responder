ec3fd38326740a1451fe37b704610483c99a7521
Denis Sermukhamedov <den.medov@gmail.com>
2016-11-20 21:26:12 +0600

    add travis timeout

--- package.sh
+++ package.sh
@@ -2,5 +2,5 @@
 set -e -x
 
 docker build -t dmedov/android .
-docker run --tty --interactive --volume=$(pwd):/opt/workspace --workdir=/opt/workspace --rm dmedov/android  /bin/sh -c "run-tests-emulator.sh"
+docker run --interactive --volume=$(pwd):/opt/workspace --workdir=/opt/workspace --rm dmedov/android  /bin/sh -c "run-tests-emulator.sh"
 
